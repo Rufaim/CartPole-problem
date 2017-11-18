c2533adae82a3868d991a34e670120f8afad9c28
Rufaim <pavelrumyancew@yandex.ru>
2017-11-18 17:27:08 +0300

    training of model

--- model_train.py
+++ model_train.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
+import numpy as np
 import gym
 from nn_layers import DenseLayer, DenseLayerConcat
 from actor_critic_deep_q_learning import ActorCriticModel
 
 
 # Train constants
-MINIBATCH_SIZE = 64
+MINIBATCH_SIZE = 200
 MAX_EPISODES = 5000
 MAX_EP_STEPS = 1000
 
@@ -19,20 +20,26 @@ ACTION_BOUNDS = 1
 CRITIC_NET_STRUCTURE = [DenseLayer(150,tf.nn.relu),DenseLayerConcat(300,tf.nn.relu),DenseLayer(10,tf.nn.relu)]
 ACTOR_NET_STRUCTURE = [DenseLayer(150,tf.nn.relu),DenseLayer(100,tf.nn.relu)]
 
-def build_summaries():
-	episode_reward = tf.Variable(0.)
-	tf.summary.scalar("Reward", episode_reward)
-	episode_ave_max_q = tf.Variable(0.)
-	tf.summary.scalar("Qmax Value", episode_ave_max_q)
 
-	summary_vars = [episode_reward, episode_ave_max_q]
+def build_summaries():
+	cum_episode_reward = tf.Variable(0.)
+	tf.summary.scalar("Cumulative reward", cum_episode_reward)
+
+	episode_length = tf.Variable(0.)
+	tf.summary.scalar("Episode length", episode_length)
+	
+	summary_vars = [cum_episode_reward,episode_length]
 	summary_ops = tf.summary.merge_all()
 
 	return summary_ops, summary_vars
 
+
 with tf.Session() as sess:
 	
 	env = gym.make("CartPole-v1")
+
+	summary_ops, summary_vars = build_summaries()
+	writer = tf.summary.FileWriter("logdir", sess.graph)
 
 	state_dim = env.observation_space.shape[0]
 	action_dim = env.action_space.shape[0]
@@ -44,5 +51,30 @@ with tf.Session() as sess:
 
 	model.initialazer()
 
-	
+	for i in range(MAX_EPISODES):
+		state = env.reset()
+		ep_reward = 0
+
+		for j in range(MAX_EP_STEPS):
+			a = model.forward_pass(np.reshape(state, (1, state_dim)))
+			next_state, r, terminal, info = env.step(a[0])
+			model.add_to_buffer(state,a[0],r,terminal,next_state)
+
+			model.update()
+
+			state = next_state
+			ep_reward += r
+
+			if terminal:
+				summary_str = sess.run(summary_ops, feed_dict={
+					summary_vars[0]: ep_reward,
+					summary_vars[1]: j
+				})
+
+				writer.add_summary(summary_str, i)
+				writer.flush()
+
+				print('| Reward: {:.2f} | Episode: {:d} | Qmax: {:.4f}'.format(ep_reward, i))
+				break
+
 	env.close()
