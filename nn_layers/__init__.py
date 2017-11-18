ccdafdcd9895e508fa49d9a2c0cfe7fc7480e226
Rufaim <pavelrumyancew@yandex.ru>
2017-11-19 01:41:22 +0300

    batch normalization

--- nn_layers/__init__.py
+++ nn_layers/__init__.py
@@ -1,8 +1,10 @@
 from .nets_constructors import DenseLayer, DenseLayerConcat
+from .normalization import BatchNormalization
 from .interfaces import Layer, LayerType
 
 __all__ = ["Layer",
 			"LayerType",
 			"DenseLayer",
-			"DenseLayerConcat"
+			"DenseLayerConcat",
+			"BatchNormalization"
 			]
---
+++ nn_layers/normalization.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+class BatchNormalization(object):
+	def __init__(self,gamma=1.0,beta=0.0,gamma_stddev=0.02,
+				epsilon=1e-5,axis=[0],name=None):
+
+		self.beta_init = tf.constant_initializer(beta)
+		self.gamma_init = tf.random_normal_initializer(mean=gamma, stddev=gamma_stddev)
+		self.epsilon = epsilon
+		self.axis = axis
+		self.name = name
+	
+	def  __call__(self, scope, input):
+		TShape = input.shape
+
+		with tf.variable_scope(scope):
+			beta = tf.Variable(self.beta_init(TShape[-1:]))
+			gamma = tf.Variable(self.gamma_init(TShape[-1:]))
+
+			mean, variance = tf.nn.moments(input, self.axis)
+			out = tf.nn.batch_normalization(input, mean, variance, beta, gamma, self.epsilon)
+			return out, [beta, gamma]
